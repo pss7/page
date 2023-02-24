d20540721c0c9b397ac4ca2e8e7ee29b4c009401
pss7 <124497122+pss7@users.noreply.github.com>
2023-02-24 14:42:22 +0900

    Add files via upload

--- js/sub.js
+++ js/sub.js
@@ -1,5 +1,6 @@
 $(function () {
 
+    /* com_review_wrap */
     $('.com_review_wrap .slick').slick({
         arrows: true,
         accessibility: false,
@@ -9,6 +10,13 @@ $(function () {
         nextArrow: $('.com_review_wrap .control .next'),
         autoplaySpeed: 5000,
         speed: 500,
+    });
+
+    /* reservation */
+    $('.reservation_box .time_lsit li a').click(function () {
+        $('.reservation_box .time_lsit li a').removeClass('active');
+        $(this).addClass('active');
+        return false;
     });
 
 });
